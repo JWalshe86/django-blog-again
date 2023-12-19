9446bb4ff5dcafd4f543317d24bd1689a169b57d
John Walshe <walshejohnnyw7@gmail.com>
2023-12-19 15:07:44 +0000

    waitress serves on 127..

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x_ugg*l3eg5+=v(!d#ow@5-(()#a7p-n(u+givf7@m3-y1w#aq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '.herokuapp.com', '127.0.0.1']
 
