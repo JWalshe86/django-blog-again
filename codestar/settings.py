3bed8c02845a35f31d7524afd52cc6d0c871e375
John Walshe <walshejohnnyw7@gmail.com>
2023-12-20 12:56:21 +0000

    adds default database

--- codestar/settings.py
+++ codestar/settings.py
@@ -87,13 +87,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # }
 
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+DATABASE_URL = 'postgresql://<postgresql>'
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+    # 'default': dj_database_url.config()
+# }
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
