871a5b418c69076ec5417ed94bb3417c3776268f
John Walshe <walshejohnnyw7@gmail.com>
2024-01-02 11:30:25 +0000

    enable serving of image files

--- blog/views.py
+++ blog/views.py
@@ -19,11 +19,12 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
     
     if request.method == "POST":
-        comment_form = CommentForm(data=request.POST)
+        comment_form = CommentForm(data=request.POST, instance=comment)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.post = post
+            comment.approved = False
             comment.save()
             messages.add_message(
                 request, messages.SUCCESS,
